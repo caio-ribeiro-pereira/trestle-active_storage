c93758a4386a845384a548ffc59a3707551e7cb8
Caio R. Pereira <caio.ribeiro.pereira@gmail.com>
2025-07-24 13:18:29 -0300

    removed logs

--- lib/trestle/active_storage/builder.rb
+++ lib/trestle/active_storage/builder.rb
@@ -2,7 +2,6 @@ module Trestle
   module ActiveStorage
     module Builder
       def active_storage_fields(&block)
-        puts "BUILDER BLOCK: #{block.inspect}\n\n\n"
         admin.define_adapter_method(:active_storage_fields, &block)
       end
     end
--- lib/trestle/active_storage/field.rb
+++ lib/trestle/active_storage/field.rb
@@ -5,16 +5,10 @@ module Trestle
         instance = builder.object
         attachment = instance.send(name)
 
-        puts "INSTANCE: #{instance.inspect}\n\n"
-        puts "BUILDER: #{builder.inspect}\n\n"
-
         rendering_options = {}.tap do |hash|
           hash[:locals] = {}.tap do |locals|
             locals[:builder] = builder
             locals[:field_name] = name
-
-            puts "HASH: #{hash.inspect}\n\n"
-            puts "LOCALS: #{locals.inspect}\n\n"
 
             if attachment.respond_to?(:each)
               hash[:partial] = 'trestle/active_storage/has_many_field'
@@ -26,9 +20,6 @@ module Trestle
           end
         end
 
-        puts "RENDERING_OPTIONS: #{rendering_options.inspect}\n\n"
-        puts "TEMPLATE #{@template.inspect}\n\n"
-        puts "==========\n\n\n"
         @template.render rendering_options
       end
     end
