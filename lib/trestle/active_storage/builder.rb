4e81b399bfa537e10a29c1280671bcb0bc9b0162
Caio R. Pereira <caio.ribeiro.pereira@gmail.com>
2025-07-24 12:53:58 -0300

    more logs...

--- lib/trestle/active_storage/builder.rb
+++ lib/trestle/active_storage/builder.rb
@@ -2,6 +2,7 @@ module Trestle
   module ActiveStorage
     module Builder
       def active_storage_fields(&block)
+        puts "BUILDER BLOCK: #{block.inspect}\n\n\n"
         admin.define_adapter_method(:active_storage_fields, &block)
       end
     end
